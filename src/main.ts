5ff2801d70b4673292444fb50a6f1c74931bb057
Manoj Khatri <90838977+RmNj17@users.noreply.github.com>
2023-05-26 06:59:30 +0545

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+// import express from 'express';
+
+// const app = express();
+
+// app.get('/', (_req, _res) => {
+//   _res.send('Hello world');
+// });
+
+// // Server setup
+// app.listen(3000, () => {
+//   console.log('HELLO WORLD');
+// });
+
+// const add = (a: number, b: number): number => a + b;
+// console.log(add(2, 3));
+
+console.log('Hello world!');
