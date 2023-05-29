8c4ecc6ac9a33bebafba08fc152bce3590a7167a
Sujhav Maskey <sujhavmaskeyy@gmail.com>
2023-05-29 23:04:20 +0545

    added contents

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,22 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express';
+import {
+  addUsers,
+  deleteUsers,
+  getUsers,
+  updateUsers,
+} from '../controllers/user.controller';
+import { createPostDto } from '../validators/user.validators';
+import { validate } from '../utils/validate';
 const router = Router();
 
-router.use('/', (req, res, next) => {
-  console.log('username: ');
-  res.send('id:its just a demo');
-});
+router.get('/', getUsers);
+
+router.post('/', validate(createPostDto), addUsers);
+
+router.delete('/:id', deleteUsers);
+router.patch('/:id', updateUsers);
+
+// router.post('/signup',)
+
+export default router;
--- src/utils/create-server.ts
+++ src/utils/create-server.ts
@@ -1,7 +1,8 @@
 import { config } from 'dotenv';
 import cors from 'cors';
-
+import router from '../routes/index';
 import express from 'express';
+import * as ErrorMiddlewares from '../middleware/errors.middleware';
 
 config();
 
@@ -9,5 +10,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use('/api', router);
+
+app.use(ErrorMiddlewares.methodNotAllowed);
+app.use(ErrorMiddlewares.genericErrorHandler);
 
 export default app;
